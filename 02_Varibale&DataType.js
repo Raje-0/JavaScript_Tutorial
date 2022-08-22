1ad0d372eb5a2d7300e36283544abfe8d9c35487
kanhaiya kumar <97101583+Raje-0@users.noreply.github.com>
2022-08-22 09:07:18 +0530

    26 video

--- 02_Varibale&DataType.js
+++ 02_Varibale&DataType.js
@@ -1,6 +1,7 @@
 console.log("hii")
-
+//var = let 
 let a=67
+
 console.log(a)
 a="raje"
 console.log(a)
---
+++ 25_Console.js
@@ -0,0 +1,15 @@
+console.log(console)
+
+console.error("this is jdfgjdg error")
+
+console.assert(5>53)
+console.assert(55>53)
+
+console.warn("dont smoke")
+
+console.time("forLoop")
+ for(let i=0; i<5;i++){
+    console.log(233)
+ }
+
+ console.timeEnd("forLoop")
